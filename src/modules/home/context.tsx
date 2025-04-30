60c2c26fef6552cb9ec779ac7211f6eb4747c510
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-04-30 23:02:54 +0100

    [feat]: add HomeContextProvider and dependencies for image generation

---
+++ src/modules/home/context.tsx
@@ -0,0 +1,52 @@
+import { AxiosClient } from "@/components";
+import React, { createContext, useContext, useState } from "react";
+
+interface IHomeState {
+  loading: boolean;
+  image: string | null;
+  generateImage: (prompt: string) => Promise<any>;
+}
+
+export const HomeContext = createContext<IHomeState | undefined>(undefined);
+
+export const useHomeState = () => {
+  const context = useContext(HomeContext);
+
+  if (context === undefined) {
+    throw new Error("useHomeState must be used within a App global Provider");
+  }
+};
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const HomeContextProvider: React.FC<IProps> = ({ children }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const generateImage = async (prompt: string) => {
+    setLoading(true);
+    try {
+      const response = await AxiosClient.post(`/image/generate`, { prompt });
+      const image = response?.data?.data;
+      console.log({ image });
+      if (image) {
+        setImage(image);
+        return image;
+      }
+      return null;
+    } catch (error: any) {
+      const err = error?.response?.data?.message || "Failed to generate image";
+      console.log({ err });
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <HomeContext.Provider value={{ loading, image, generateImage }}>
+      {children}
+    </HomeContext.Provider>
+  );
+};
