f2a76d227388c39a69823b111628ca5db5241c13
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-12 09:46:05 +0100

    Update card.tsx

--- src/modules/features/components/card.tsx
+++ src/modules/features/components/card.tsx
@@ -4,12 +4,17 @@ export const FeatureCard = ({
   icon,
   title,
   desc,
+  link
 }: {
   icon: string;
   title: string;
   desc: string;
+link: string
+
 }) => {
   return (
+    <Link href={link}>
+    
     <motion.div
       className="bg-gradient-to-r from-blue-100 to-gray-100 border-l-4 border-orange-400 p-6 shadow-sm rounded-lg"
       whileHover={{ scale: 1.03 }}
@@ -22,5 +27,7 @@ export const FeatureCard = ({
       <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
       <p className="text-gray-600 mt-2 text-sm">{desc}</p>
     </motion.div>
+      
+    </Link>
   );
 };
