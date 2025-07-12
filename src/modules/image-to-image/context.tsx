27bb5c5a83b97a579d3c0f3db71f7d4f1077a420
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-12 11:17:00 +0100

    Update context.tsx

--- src/modules/image-to-image/context.tsx
+++ src/modules/image-to-image/context.tsx
@@ -5,7 +5,7 @@ import { toast } from "react-hot-toast";
 interface IImageToImageState {
   loading: boolean;
   image: string | null;
-  generateImage: (prompt: string, image: { uri: string; name: string; type: string }) => Promise<any>;
+  generateImage: (prompt?: string, image: { uri: string; name: string; type: string }) => Promise<any>;
 }
 
 const ImageToImageContext = createContext<IImageToImageState | undefined>(undefined);
@@ -14,7 +14,7 @@ export const ImageToImageProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [image, setImage] = useState<string | null>(null);
 
-  const generateImage = async (prompt: string, inputImage: { uri: string; name: string; type: string }) => {
+  const generateImage = async (prompt?: string, inputImage: { uri: string; name: string; type: string }) => {
     setLoading(true);
     try {
       const res = await AxiosClient.post("/transform/image", { prompt, image: inputImage });
